6ec782aa7df724c88ed97f04526c526d97daad15
PH Tools <phtools@bldgtyp.com>
2024-09-16 16:13:54 +0200

    refactor measures

---
+++ honeybee_revive/CO2_measures.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+# -*- Python Version: 2.7 -*-
+
+"""HB-Model Phius REVIVE CO2-Reduction-Measure and Measure-Collection Classes."""
+
+try:
+    from typing import Iterator
+except ImportError:
+    pass  # Python 2.7
+
+try:
+    from honeybee_ph_utils.enumerables import CustomEnum
+except ImportError as e:
+    raise ImportError("\nFailed to import honeybee_ph_utils:\n\t{}".format(e))
+
+
+class CO2ReductionMeasureType(CustomEnum):
+    allowed = [
+        "PERFORMANCE",
+        "NON_PERFORMANCE",
+    ]
+
+    def __init__(self, _value=1):
+        # type: (str | int) -> None
+        super(CO2ReductionMeasureType, self).__init__(_value, _index_offset=-1)
+
+
+class CO2ReductionMeasure(object):
+    def __init__(
+        self,
+        name="unnamed_CO2_measure",
+        measure_type="PERFORMANCE",
+        year=60,
+        cost=8500.0,
+        kg_CO2=0.0,
+        country_name="USA",
+        labor_fraction=0.4,
+    ):
+        self.name = name
+        self.measure_type = CO2ReductionMeasureType(measure_type)
+        self.year = year
+        self.cost = cost
+        self.kg_CO2 = kg_CO2
+        self.country_name = country_name
+        self.labor_fraction = labor_fraction
+
+    @property
+    def unique_id(self):
+        # type: () -> str
+        return "{}-{}-{}-{}-{}".format(self.name, self.measure_type.number, self.year, self.cost, self.labor_fraction)
+
+    def to_dict(self):
+        # type: () -> dict
+        d = {}
+        d["measure_type"] = self.measure_type.to_dict()
+        d["name"] = self.name
+        d["year"] = self.year
+        d["cost"] = self.cost
+        d["kg_CO2"] = self.kg_CO2
+        d["country_name"] = self.country_name
+        d["labor_fraction"] = self.labor_fraction
+        return d
+
+    @classmethod
+    def from_dict(cls, _dict):
+        # type: (dict) -> CO2ReductionMeasure
+        measure = cls()
+        measure.measure_type = CO2ReductionMeasureType.from_dict(_dict["measure_type"])
+        measure.name = _dict["name"]
+        measure.year = _dict["year"]
+        measure.cost = _dict["cost"]
+        measure.kg_CO2 = _dict["kg_CO2"]
+        measure.country_name = _dict["country_name"]
+        measure.labor_fraction = _dict["labor_fraction"]
+        return measure
+
+    def duplicate(self):
+        # type: () -> CO2ReductionMeasure
+        return CO2ReductionMeasure.from_dict(self.to_dict())
+
+    def __copy__(self):
+        # type: () -> CO2ReductionMeasure
+        return self.duplicate()
+
+    def __str__(self):
+        # type: () -> str
+        return "{}(name={})".format(self.__class__.__name__, self.name)
+
+    def __repr__(self):
+        # type: () -> str
+        return str(self)
+
+    def ToString(self):
+        # type: () -> str
+        return str(self)
+
+
+class CO2ReductionMeasureCollection(object):
+    def __init__(self):
+        self._storage = {}  # type: dict[str, CO2ReductionMeasure]
+
+    def add_measure(self, measure):
+        # type: (CO2ReductionMeasure) -> None
+        self._storage[measure.unique_id] = measure
+
+    def measures(self):
+        # type: () -> list[CO2ReductionMeasure]
+        return list(self._storage.values())
+
+    def keys(self):
+        # type: () -> list[str]
+        return [k for k, v in sorted(self._storage.items(), key=lambda x: x[1].unique_id)]
+
+    def values(self):
+        # type: () -> list[CO2ReductionMeasure]
+        return list(sorted(self._storage.values(), key=lambda x: x.unique_id))
+
+    def duplicate(self):
+        # type: () -> CO2ReductionMeasureCollection
+        new_collection = CO2ReductionMeasureCollection()
+        for measure in self.measures():
+            new_collection.add_measure(measure.duplicate())
+        return new_collection
+
+    def __copy__(self):
+        # type: () -> CO2ReductionMeasureCollection
+        return self.duplicate()
+
+    def to_dict(self):
+        # type: () -> dict[str, dict]
+        return {k: v.to_dict() for k, v in self._storage.items()}
+
+    @classmethod
+    def from_dict(cls, _dict):
+        # type: (dict) -> CO2ReductionMeasureCollection
+        collection = cls()
+        for k, v in _dict.items():
+            collection.add_measure(CO2ReductionMeasure.from_dict(v))
+        return collection
+
+    def __iter__(self):
+        # type: () -> Iterator[CO2ReductionMeasure]
+        return iter(sorted(self._storage.values(), key=lambda x: x.unique_id))
+
+    def __contains__(self, key):
+        # type: (str | CO2ReductionMeasure) -> bool
+        if isinstance(key, CO2ReductionMeasure):
+            return key in self._storage.values()
+        return key in self._storage
+
+    def __len__(self):
+        # type: () -> int
+        return len(self._storage)
+
+    def __str__(self):
+        # type: () -> str
+        return "{}({}-measures)".format(self.__class__.__name__, len(self._storage))
+
+    def __repr__(self):
+        # type: () -> str
+        return str(self)
+
+    def ToString(self):
+        # type: () -> str
+        return str(self)
--- honeybee_revive/properties/model.py
+++ honeybee_revive/properties/model.py
@@ -28,6 +28,7 @@ except ImportError as e:
 try:
     from honeybee_revive.grid_region import GridRegion
     from honeybee_revive.national_emissions import NationalEmissionsFactors
+    from honeybee_revive.CO2_measures import CO2ReductionMeasureCollection
 
     if TYPE_CHECKING:
         from honeybee_revive.properties.aperture import ApertureReviveProperties
@@ -46,6 +47,7 @@ class ModelReviveProperties(object):
         self.national_emissions_factors = NationalEmissionsFactors()
         self.analysis_duration = 50
         self.envelope_labor_cost_fraction = 0.4
+        self.co2_measures = CO2ReductionMeasureCollection()
 
     @property
     def host(self):
@@ -85,6 +87,7 @@ class ModelReviveProperties(object):
         new_properties_obj.national_emissions_factors = self.national_emissions_factors.duplicate()
         new_properties_obj.analysis_duration = self.analysis_duration
         new_properties_obj.envelope_labor_cost_fraction = self.envelope_labor_cost_fraction
+        new_properties_obj.co2_measures = self.co2_measures.duplicate()
 
         return new_properties_obj
 
@@ -127,6 +130,7 @@ class ModelReviveProperties(object):
         d["national_emissions_factors"] = self.national_emissions_factors.to_dict()
         d["analysis_duration"] = self.analysis_duration
         d["envelope_labor_cost_fraction"] = self.envelope_labor_cost_fraction
+        d["co2_measures"] = self.co2_measures.to_dict()
 
         return {"revive": d}
 
@@ -154,12 +158,13 @@ class ModelReviveProperties(object):
         new_prop.national_emissions_factors = NationalEmissionsFactors.from_dict(_dict["national_emissions_factors"])
         new_prop.analysis_duration = _dict["analysis_duration"]
         new_prop.envelope_labor_cost_fraction = _dict["envelope_labor_cost_fraction"]
+        new_prop.co2_measures = CO2ReductionMeasureCollection.from_dict(_dict["co2_measures"])
 
         return new_prop
 
     @staticmethod
     def load_properties_from_dict(data):
-        # type: (dict[str, dict]) -> tuple[GridRegion, NationalEmissionsFactors, int, float]
+        # type: (dict[str, dict]) -> tuple[GridRegion, NationalEmissionsFactors, int, float, CO2ReductionMeasureCollection]
         """Load the HB-Model .revive properties from an HB-Model dictionary as Python objects.
 
         The function is called when re-serializing an HB-Model object from a
@@ -182,8 +187,15 @@ class ModelReviveProperties(object):
         )
         analysis_duration = data["properties"]["revive"]["analysis_duration"]
         envelope_labor_cost_fraction = data["properties"]["revive"]["envelope_labor_cost_fraction"]
+        measures_collection = CO2ReductionMeasureCollection.from_dict(data["properties"]["revive"]["co2_measures"])
 
-        return (grid_region, national_emissions_factors, analysis_duration, envelope_labor_cost_fraction)
+        return (
+            grid_region,
+            national_emissions_factors,
+            analysis_duration,
+            envelope_labor_cost_fraction,
+            measures_collection,
+        )
 
     def apply_properties_from_dict(self, data):
         # type: (dict) -> None
@@ -217,9 +229,13 @@ class ModelReviveProperties(object):
         ) = extensionutil.model_extension_dicts(data, "revive", [], [], [], [], [])
 
         # re-build all of the .revive property objects from the HB-Model dict as python objects
-        self.grid_region, self.national_emissions_factors, self.analysis_duration, self.envelope_labor_cost_fraction = (
-            self.load_properties_from_dict(data)
-        )
+        (
+            self.grid_region,
+            self.national_emissions_factors,
+            self.analysis_duration,
+            self.envelope_labor_cost_fraction,
+            self.co2_measures,
+        ) = self.load_properties_from_dict(data)
 
         # apply the .revive properties to all the sub-model objects in the HB-Model
         for room, room_dict in zip(self.host_rooms, room_revive_dicts):
